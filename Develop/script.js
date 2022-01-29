a510700e3eb79fb7244ed75b4dd2beb228db1563
Jack Parker <jackhparker98@gmail.com>
2022-01-28 22:55:23 -0500

    the 2nd draft just need random math to pull from array

--- Develop/script.js
+++ Develop/script.js
@@ -12,11 +12,12 @@ var numbers = [`1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `0`];
 var upperCase = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`];
 var special = [`!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `(`, `)`, `_`, `+`, `-`, `=`, `~`];
 
+var genArry = undefined;
+
+
 function generatePassword() {
 var userImput =  parseInt(prompt("Please eneter the desiered length of password between 8 and 128 characters."));
-
 // this is the lenght part to the password
-
 if (8 > userImput || userImput > 128) {
   window.alert("Please enter a valid number between 8 and 128.")
 } else {
@@ -24,39 +25,88 @@ passwordlength = userImput;
   window.alert("your password will be " + passwordlength + " characters long.")
 };
 console.log(userImput);
-
 // this is the numbers prompt
 userNumbers = window.confirm("Would you like to have numbers?");
 console.log(userNumbers);
-
 // this is the special prompt
-userSpecial = window.confirm("Would you like to have Specail characters?");
+userSpecial = window.confirm("Would you like to have Special characters?");
 console.log(userSpecial);
-
 // this is the lowercase
 userLowerCase = window.confirm("Would you like to have Lower case characters?");
 console.log(userLowerCase);
-
 // this is the uppercase
 userUpperCase = window.confirm("Would you like to have Upper case characters?");
 console.log(userUpperCase);
 
 
+  
+  // this is the numbers prompt
+if (userNumbers && userLowerCase && userUpperCase && userSpecial) {
+    genArry = numbers.concat(lowerCase, upperCase, special);
+
+  } else if (!userNumbers && userLowerCase && userUpperCase && userSpecial) {
+    genArry = lowerCase.concat(upperCase, special);
+
+  } else if (!userNumbers && !userLowerCase && userUpperCase && userSpecial) {
+    genArry = upperCase.concat(special);
+
+  } else if (!userNumbers && !userLowerCase && !userUpperCase && userSpecial) {
+    genArry = (special);
+  
+  } else if (userNumbers && !userLowerCase && userUpperCase && userSpecial) {
+    genArry = numbers.concat(upperCase, special);
+
+  } else if (userNumbers && userLowerCase && userUpperCase && !userSpecial) {
+    genArry = numbers.concat(lowerCase, upperCase);
+
+  } else if (userNumbers && userLowerCase && !userUpperCase && !userSpecial) {
+    genArry = numbers.concat(lowerCase);
+
+  } else if (!userNumbers && userLowerCase && userUpperCase && !userSpecial) {
+    genArry = numbers.concat(special);
+
+  } else if (!userNumbers && userLowerCase && !userUpperCase && userSpecial) {
+    genArry = lowerCase.concat(special);
+
+  } else if (userNumbers && !userLowerCase && userUpperCase && !userSpecial) {
+    genArry = numbers.concat(upperCase);
+
+  } else if (userNumbers && !userLowerCase && !userUpperCase && userSpecial) {
+    genArry = numbers.concat(special);
+
+  } else if (userNumbers && !userLowerCase && !userUpperCase && !userSpecial) {
+    genArry = (numbers);
+
+  } else if (!userNumbers && userLowerCase && !userUpperCase && !userSpecial) {
+    genArry = (lowerCase);
+
+  } else if(!userNumbers && !userLowerCase && userUpperCase && !userSpecial) {
+    genArry = (upperCase);
+  }
+  console.log(genArry);
 
 
 
-
-
-
-
-
-
-
-
-
-
-return;
+  // for (i = 0; i < placeholder; 1++) {
+  //   //math random for the pulling randomly for the newest array for the pass ward
+  // }
+  return;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
